df16b50ef67714154350a9bd42803ccdb41dd486
Nguyễn Hồng Chương <chuongnhd00645@fpt.edu.vn>
2020-08-19 00:50:49 +0700

    Test deploy

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>ChuongNH</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to ReactJS project!
         </p>
         <a
           className="App-link"
