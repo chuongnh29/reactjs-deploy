d144fa02736ea8c00aad77d2f54a363121876ee5
Nguyễn Hồng Chương <chuongnhd00645@fpt.edu.vn>
2020-08-19 00:48:10 +0700

    Updates

---
+++ precache-manifest.9a53fb823d6dfc7a63f8d96aaadb702e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4d9e43e3bb6994a8126eea941da942",
+    "url": "/reactjs-deploy/index.html"
+  },
+  {
+    "revision": "e586b0b95f4e9b483d1f",
+    "url": "/reactjs-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "27dad8e39f08b80cb2e9",
+    "url": "/reactjs-deploy/static/js/2.3c7e66fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-deploy/static/js/2.3c7e66fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e586b0b95f4e9b483d1f",
+    "url": "/reactjs-deploy/static/js/main.d046e451.chunk.js"
+  },
+  {
+    "revision": "98248677f261da5e461d",
+    "url": "/reactjs-deploy/static/js/runtime-main.6c08982c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
